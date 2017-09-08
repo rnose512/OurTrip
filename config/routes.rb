cdd4b4b807b08173c06b299c527a1ff2ed6ba0d6
Ashley Teow <ashleyteow@Ashleys-MacBook-Air.local>
2017-09-07 17:26:23 -0700

    Create all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :trips do
+    resources :packing_lists do
+      resources :items, only: [:new, :create, :delete]
+    end
+    resources :expenses
+    resources :destinations do
+      resources :events, :accomodations, :transportations
+    end
+  end
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'register', to: 'users#new'
+  post 'register', to: 'users#create'
+
 end
