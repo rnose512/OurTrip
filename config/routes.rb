c13195ce103c3a42521275d1b0b1d4f4d7758974
Ashley Teow <ashleyteow@Ashleys-MacBook-Air.local>
2017-09-07 18:22:24 -0700

    Nest appropriate routes according to necessity

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   resources :trips do
-    resources :packing_lists do
-      resources :items, only: [:new, :create, :delete]
-    end
+    resources :packing_lists, only: [:index]
     resources :expenses
-    resources :destinations do
-      resources :events, :accomodations, :transportations
-    end
+    resources :destinations, only: [:new, :create, :destroy, :index]
+  end
+
+  resources :packing_lists, only: [:index] do
+    resources :items, only: [:new, :create, :destroy]
+  end
+
+
+  resources :destinations, except: [:new, :create, :destroy, :show, :index] do
+    resources :events
+    resources :accomodations, except: [:show]
+    resources :transportations, except: [:show]
   end
 
   get 'login', to: 'sessions#new'
