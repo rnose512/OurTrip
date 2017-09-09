2bbdecade7fcbc123c575cb57148d02cb863e424
Hawken Ritter <hawkenritter@gmail.com>
2017-09-09 12:21:40 -0700

    Moves TripList into components folder.  Adds axios and makes axios get request to localhost for trips.  Outputs trips to OurTrip index

--- OurTripReact/index.ios.js
+++ OurTripReact/index.ios.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { AppRegistry, View, Text } from 'react-native';
+import TripList from './src/components/TripList';
 
 class OurTrip extends Component {
   render() {
+    console.log("WTFFFF")
     return (
       <View>
-        <Text> HELLLOOOOOO </Text>
+        <TripList />
       </View>
     );
   }
--- OurTripReact/src/components/TripList.js
+++ OurTripReact/src/components/TripList.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class TripList extends Component {
   state = { trips: [] };
 
   componentWillMount() {
-
-    make http request
+    console.log("ARE WE HITTING THE COMPONENET MOUNT?")
+    axios.get('http://localhost:3000/trips')
+    .then(response => this.setState({ trips: response.data }));
   }
 
   renderTrips() {
     return this.state.trips.map(trip =>
-      //render trip display component
+      <Text>{trip.name}</Text>
     );
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         {this.renderTrips()}
       </View>
     );
-
   }
-
 }
+export default TripList;
