79eb31325a4a97a1833fb44eebe915f2b395069f
Hannie Dong <hdong92@gmail.com>
2017-09-10 12:07:17 -0700

    Fix login feature

--- OurTripReact/index.ios.js
+++ OurTripReact/index.ios.js
@@ -2,14 +2,13 @@
 import React, { Component } from 'react';
 import { AppRegistry, View, Text, StyleSheet } from 'react-native';
 import RouterComponent from './src/Router';
-import Dock from './src/components/common/Dock';
 
 class OurTrip extends Component {
   render() {
     return (
       <View style={styles.container} >
         <RouterComponent style={styles.routes} />
-        <Dock style={styles.dock} />
+
       </View>
     );
   }
