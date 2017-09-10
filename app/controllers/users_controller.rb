f0ccedf4610d4cb89b3b7b0eed396a152a11821c
Hannie Dong <hdong92@gmail.com>
2017-09-10 15:15:19 -0700

    Modify controllers for user login and registration.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,17 @@
 class UsersController < ApplicationController
 
-  def new
-    @user = User.new
-  end
-
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
-      if @user.save
-        render json: {
-          accessToken: @user.access_token
-        }.to_json
-      else
-        render json: {
-          errors: ['Could not create the user']
-        }.to_json
-      end
+    user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], phone_number: params[:phone_number], emergency_contact: params[:emergency_contact], emergency_contact_phone_number: params[:emergency_contact_phone_number])
+    if user.save
+      render json: {
+        saved: true,
+        accessToken: user.access_token
+      }.to_json
+    else
+      render json: {
+        saved: false
+      }.to_json
+    end
   end
 
 end
