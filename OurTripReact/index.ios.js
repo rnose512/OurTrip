e3cbd2eed598df0e427c3a92865359032c232083
Peter Thompson <peterthompson@roamflyfishing.com>
2017-09-10 11:32:47 -0700

    Creates dock component and adds flex styling to index.ios

--- OurTripReact/index.ios.js
+++ OurTripReact/index.ios.js
@@ -1,16 +1,31 @@
 //MAIN PAGE
 import React, { Component } from 'react';
-import { AppRegistry, View, Text } from 'react-native';
-import RouterComponent from './src/Router'
+import { AppRegistry, View, Text, StyleSheet } from 'react-native';
+import RouterComponent from './src/Router';
+import Dock from './src/components/common/Dock';
 
 class OurTrip extends Component {
   render() {
     return (
-      <View>
-        <RouterComponent />
+      <View style={styles.container} >
+        <RouterComponent style={styles.routes} />
+        <Dock style={styles.dock} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   flexDirection: 'column',
+  },
+  routes: {
+    flex: 5,
+  },
+  dock: {
+    flex: 2,
+  }
+})
 
 AppRegistry.registerComponent('OurTripReact', () => OurTrip)
