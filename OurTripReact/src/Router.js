660346bc07b5de16cf95c7c00f353af6bffc892e
Hawken Ritter <hawkenritter@gmail.com>
2017-09-09 19:51:57 -0700

    fix bugs after massive merge

--- OurTripReact/src/Router.js
+++ OurTripReact/src/Router.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import TripShow from './components/TripShow.js';
 import Trips from './components/Trips';
-import Itinerary from './Itinerary'
+import Itinerary from './Itinerary';
+import App from './app';
 
 const RouterComponent = () => {
   return (
     <Router>
+      <Scene key="App" component={App} initial />
       <Scene key='TripList' component={Trips} />
       <Scene key='TripShow' component={TripShow} />
       <Scene key='Itinerary' component={Itinerary} />
